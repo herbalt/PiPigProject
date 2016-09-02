45fb7a19f95aa07d63b1b7372af8f743aa8578b2
herbalt <tajmckenzie@gmail.com>
2016-09-02 08:36:31 +0100

    Blanked Credentials

--- credentials/credentials.py
+++ credentials/credentials.py
@@ -1,20 +1,20 @@
 OAUTH_CREDENTIALS = {
     'facebook': {
-        'id': '494803964049198',
-        'secret': '8e8a243c9d78696161cb7795075127eb'
+        'id': '4xxxxxxxxxxxxx',
+        'secret': '8xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
     },
     'twitter': {
-        'id': '3RzWQclolxWZIMq5LJqzRZPTl',
-        'secret': 'm9TEd58DSEtRrZHpz2EjrV9AhsBRxKMo8m3kuIZj3zLwzwIimt'
+        'id': '3xxxxxxxxxxxxxxxxxxxxxxxxx',
+        'secret': 'mxxxxxxxxxxxxxxxxxxxxxxxx'
     }
 }
 
 SECRET_KEY = 'SECRET_KEY'
 SECURITY_PASSWORD_SALT = 'SECURITY_PASSWORD_SALT'
 
-# APP EMAIL CREDENTIALS
-MAIL_USERNAME = 'pipigdeveloper'
-MAIL_PASSWORD = 'notsosecret'
+# APP EMAIL CREDENTIAL
+MAIL_USERNAME = 'xxxxxxxxxxxxxx'
+MAIL_PASSWORD = 'xxxxxxxxxxxxxx'
 
 # MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
 # MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
